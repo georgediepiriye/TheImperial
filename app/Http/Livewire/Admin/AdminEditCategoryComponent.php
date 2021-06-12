1906ce0556bacccd9b329d6614caf4d42d1bb92a
George Diepiriye <georgediepiriye4u@gmail.com>
2021-06-12 08:46:32 +0100

    worked on edit category functionality

---
+++ app/Http/Livewire/Admin/AdminEditCategoryComponent.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use App\Models\Category;
+use Livewire\Component;
+use Illuminate\Support\Str;
+
+class AdminEditCategoryComponent extends Component
+{
+    public $category_slug,$category_id,$name,$slug;
+
+public function mount($category_slug){
+    $category = Category::where('slug',$category_slug)->first();
+    $this->category_id = $category->id;
+    $this->name=$category->name;
+    $this->slug= $category->slug;
+
+}
+
+public function updateCategory(){
+    $category = Category::find($this->category_id);
+    $category->name = $this->name;
+    $category->slug = $this->slug;
+    $category->save();
+    session()->flash('message','Category Updated Successfully');
+}
+
+public function generateSlug(){
+    $this->slug = Str::slug($this->name);
+}
+
+    public function render()
+    {
+        
+        return view('livewire.admin.admin-edit-category-component')->layout('layouts.base');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\Admin\AdminCategoryComponent;
 use App\Http\Livewire\Admin\AdminAddCategoryComponent;
+use App\Http\Livewire\Admin\AdminEditCategoryComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -48,5 +49,6 @@ Route::middleware(['auth:sanctum', 'verified','authAdmin'])->group(function(){
     Route::get('/admin/dashboard',AdminDashboardComponent::class)->name('admin.dashboard');
     Route::get('/admin/categories',AdminCategoryComponent::class)->name('admin.categories');
     Route::get('/admin/category/add',AdminAddCategoryComponent::class)->name('admin.addcategory');
+    Route::get('/admin/category/edit/{category_slug}',AdminEditCategoryComponent::class)->name('admin.editcategory');
 
 });
