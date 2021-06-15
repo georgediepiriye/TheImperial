8f89a1f1e27269a6f7eb7e939a03a2ee9894965f
George Diepiriye <georgediepiriye4u@gmail.com>
2021-06-15 15:18:07 +0100

    worked on admin delete product confirmation

--- resources/views/livewire/admin/admin-category-component.blade.php
+++ resources/views/livewire/admin/admin-category-component.blade.php
@@ -52,7 +52,7 @@
                                         <td>{{ $category->slug }}</td>
                                         <td>
                                             <a href="{{ route('admin.editcategory',['category_slug'=>$category->slug]) }}"> <i  class="fa fa-edit fa-2x"></i></a>
-                                            <a href="#" style="margin-left: 10px;" wire:click.prevent="deleteCategory({{ $category->id }})"> <i  class="fa fa-times fa-2x text-danger"></i></a>
+                                            <a href="#" style="margin-left: 10px;" onclick="confirm('Are you sure you want to delete this category?')|| event.stopImmediatePropagation()"  wire:click.prevent="deleteCategory({{ $category->id }})"> <i  class="fa fa-times fa-2x text-danger"></i></a>
                                         </td>
                                         
                                     </tr>
